d6273ea489bdb857235fdab982151d3de23988f6
Carlos <fotoscender@gmail.com>
2024-03-28 22:48:49 +0000

    Added all the comments

--- details.py
+++ details.py
@@ -2,9 +2,14 @@
 
 # Ask user for their name
 name = str(input("Your Name: "))
+# Ask user their age
 age = int(input("Your Age: ")) 
+# Ask user their house number
 house_num = int(input("Your House Number: "))
+# Ask user the street name
 street_name = str(input("Your Street Name: "))
+# Ask user their origin country
 nationality = str(input("What country are you from: "))
 
+# Print the user inputs
 print(f'This is {name}, born in {nationality}, has {age} years old. Lives at door number {house_num} on {street_name}.')
