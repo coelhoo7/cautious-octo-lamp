d47a23d62477d571f1e7213dbadc0854d1d638b2
Carlos <fotoscender@gmail.com>
2024-03-28 22:41:57 +0000

    Added comments

--- details.py
+++ details.py
@@ -1,8 +1,10 @@
 # Starting by creating some inputs to collect information from users and store them in variables
 
+# Ask user for their name
 name = str(input("Your Name: "))
 age = int(input("Your Age: ")) 
 house_num = int(input("Your House Number: "))
 street_name = str(input("Your Street Name: "))
+nationality = str(input("What country are you from: "))
 
-print(f'This is {name}, has {age} years old. Lives at door number {house_num} on {street_name}.')
+print(f'This is {name}, born in {nationality}, has {age} years old. Lives at door number {house_num} on {street_name}.')
